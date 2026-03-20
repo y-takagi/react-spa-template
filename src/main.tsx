8eca65db662bcbde51f4f78fa857faf85584518f
y-takagi <ytakagi.deb@gmail.com>
2026-03-21 02:07:56 +0900

    Fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { App } from './app.tsx';
+import { App } from './app';
 import './index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
